4a50f153bcaab58fc9e41b03fcf6e12a3b3ada30
Ascended300 <eduardofelix014@gmail.com>
2022-09-30 22:35:34 -0300

    Create components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const todos = [
+  { text: 'Pelar cebolla', completed: false},
+  { text: 'Cortar cebolla', completed: false},
+  { text: 'Poner cebolla', completed: false}
+]
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TodoCounter />
+      <h2>Has completado 2 de 3 TODOs</h2>
+      <TodoSearch />
+      <input placeholder='Cebolla' />
+  
+      <TodoList>
+        {todos.map(todo => (
+          <TodoItem />
+        ))}
+      </TodoList>
+      <CreateTodoButton />
+      <button>+</button>
+    </>
   );
 }
 
